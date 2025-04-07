fc2e3863d61209881ce10d9a27a183e32f31511c
andre <andre@AndreasHMS>
2025-04-07 15:53:22 -0300

    exercicio24

---
+++ src/main/java/com/mycompany/exercicio24/Exercicio24.java
@@ -0,0 +1,30 @@
+
+
+package com.mycompany.exercicio24;
+
+
+public class Exercicio24 {
+    public static void main(String[] args) {
+        int i = 14;
+        while(i<=22){
+            System.out.println(i);
+            i=i+2;
+        }
+    }
+}
+            
+
+       
+        
+
+
+
+        
+        
+        
+
+
+    
+
+    
+
